840b3b3e6c641dd17753cb129a945ed8336cc11a
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-02 18:59:47 -0300

    upgrade models and add admin functionality

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
+# Django imports
 from django.contrib import admin
 
+# Internal imports
+from .models import Book, Chapter
+
+# External imports
+
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Chapter)
--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 from django.utils import timezone
 
+# Internal imports
+
 # External imports
 import datetime
 
@@ -9,27 +11,25 @@ import datetime
 
 class Book(models.Model):
     # Atributes of a book
-    title = models.CharField(max_length=280)
-    long_description =  models.CharField(max_length=560)
-    short_description =  models.CharField(max_length=280)
-    created_at = models.DateTimeField('date of creation')
-    published_at = models.DateTimeField('date published')
+    title = models.CharField(max_length=280, default='My Title')
+    long_description =  models.CharField(max_length=560, default='My Long Description')
+    short_description =  models.CharField(max_length=280, default='My Short Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     # Function for recent creation of a book
     def was_created_recently(self):
         return self.created_at >= timezone.now() - datetime.timedelta(days=1)
-
-    # Function for recent publication of a book
-    def was_published_recently(self):
-        return self.published_at >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.title
 
 class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    name = models.CharField(max_length=280)
-    content = models.CharField(max_length=1000)
+    name = models.CharField(max_length=280, default='My Chapter Name')
+    content = models.CharField(max_length=1000, default='My Content For The Chapter')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
