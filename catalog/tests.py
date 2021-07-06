72cbaf052d0dfd6e7cd2a250cc5770413dc0b270
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-06 01:50:04 -0300

    add some tests for chapters

--- catalog/tests.py
+++ catalog/tests.py
@@ -3,9 +3,29 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 # Internal imports
-from .models import Book
+from .models import Book, Chapter
 # External imports
 import datetime
+
+# Function for creating a book and publishing it within a certain time offset in days
+def create_book(title, long_description, short_description, days):
+    """
+    Create a book with the given data, but sets the published date with the
+    given number of `days` offset to now (negative for books published
+    in the past, positive for books that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Book.objects.create(title=title, long_description=long_description, short_description=short_description, published_at=time)
+
+# Function for creating a chapter and publishing it within a certain time offset in days
+def create_chapter(book, name, content, score, days):
+    """
+    Create a chapter with the given data, but sets the published date with the
+    given number of `days` offset to now (negative for chapters published
+    in the past, positive for chapters that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Chapter.objects.create(book=book, name=name, content=content, score=score, published_at=time)
 
 # Tests for the model of a book
 class BookModelTests(TestCase):
@@ -36,16 +56,6 @@ class BookModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_book = Book(published_at=time)
         self.assertIs(recent_book.was_published_recently(), True)
-
-# Function for creating a book and publishing it within a certain time offset in days
-def create_book(title, long_description, short_description, days):
-    """
-    Create a book with the given data, but sets the published date with the
-    given number of `days` offset to now (negative for books published
-    in the past, positive for books that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Book.objects.create(title=title, long_description=long_description, short_description=short_description, published_at=time)
 
 # Tests for the view of the index, with many books
 class BookIndexViewTests(TestCase):
@@ -95,3 +105,15 @@ class BookIndexViewTests(TestCase):
         book2 = create_book(title="Old book 1.", long_description="Default loooong description.", short_description="Default short desc.", days=-5)
         response = self.client.get(reverse('catalog:index'))
         self.assertQuerysetEqual(response.context['latest_books_list'],[book2, book1],)
+
+    def test_book_without_chapters(self):
+        """
+        A book with no published chapters should not show any chapters
+        """
+        book1 = create_book(title="Book without chapter.", long_description="Default loooong description.", short_description="Default short desc.", days=-5)
+        chapter1 = create_chapter(book=book1, name="Unpublished chapter", content="Looots of content", score=0, days=5)
+
+        response = self.client.get(reverse('catalog:details', args=(str(book1.id))))
+
+        self.assertIn(response.context['book'].title, book1.title) # ? Weird test, not like the other assertQuerysetEqual, but technically it should have the title so it's ok?
+        self.assertQuerysetEqual(response.context['chapters'],[],)
